9ccd84ff10ec6e45a68a523a01a3fb47b32c5473
linxiao-zhang <1456597572@qq.com>
2021-12-05 10:50:50 -0800

    JVM

---
+++ src/main/java/JVM/hotspot/src/share/vm/interpreter/BaseBytecodeStream.java
@@ -0,0 +1,84 @@
+package JVM.hotspot.src.share.vm.interpreter;
+
+import JVM.hotspot.src.share.tools.DataTranslate;
+import JVM.hotspot.src.share.tools.Stream;
+import JVM.hotspot.src.share.vm.memory.StackObj;
+import JVM.hotspot.src.share.vm.oops.CodeAttributeInfo;
+import JVM.hotspot.src.share.vm.oops.MethodInfo;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class BaseBytecodeStream extends StackObj {
+    protected MethodInfo belongMethod;
+    protected CodeAttributeInfo belongCode;
+
+    protected int length;
+    protected int index;
+    protected byte[] codes;
+
+    public int getU1Code(){
+        if(index<0 || index>=length){
+            throw new Error("字节码指令的索引超过了最大值");
+        }
+        return Byte.toUnsignedInt(codes[index++]);
+
+    }
+
+    public int getU2Code(){
+        if(index<=0||index>length){
+            throw new Error("字节码指令的索引超过了最大值");
+        }
+
+        byte[] u2Arr = new byte[2];
+
+        Stream.readU2Simple(codes,index,u2Arr);
+        index+=2;
+        return DataTranslate.byteArrayToInt(u2Arr);
+    }
+    public short getUnsignedShort() {
+        if (index < 0 || index >= length) {
+            throw new Error("字节码指令的索引超过了最大值");
+        }
+
+        byte[] u2Arr = new byte[2];
+
+        Stream.readU2Simple(codes, index, u2Arr);
+
+        index += 2;
+
+        return (short) DataTranslate.byteToUnsignedShort(u2Arr);
+    }
+
+    public int getU4Code() {
+        if (index < 0 || index >= length) {
+            throw new Error("字节码指令的索引超过了最大值");
+        }
+
+        byte[] arr = new byte[4];
+
+        Stream.readU2Simple(codes, index, arr);
+        index += 4;
+
+        ByteBuffer buffer = ByteBuffer.wrap(arr);
+        buffer.order(ByteOrder.LITTLE_ENDIAN);
+
+        return buffer.getInt();
+    }
+
+    public void reset() {
+        index = 0;
+    }
+
+    public boolean end() {
+        return index >= length;
+    }
+
+    public void inc(int step) {
+        index +=  step;
+    }
+
+}
